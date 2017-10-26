a028209309b6c4021b14904ff487382c3f2497fc
Ryan Witt <onecreativenerd@gmail.com>
2017-10-25 20:37:19 -0400

    try travisized dockerized build

--- build-xenial.sh
+++ build-xenial.sh
@@ -3,6 +3,6 @@ set -xeuo pipefail
 TAG=journald-cloudwatch-logs:$(date '+%Y%m%d.%H%M%S')
 docker build -f ubuntu-xenial.dockerfile -t $TAG .
 mkdir -p bin
-docker run -it $TAG sh -c 'cat ./journald-cloudwatch-logs | base64' | base64 -D > ./bin/journald-cloudwatch-logs-xenial
+docker run -it $TAG sh -c 'cat ./journald-cloudwatch-logs | base64 -w0' | base64 --decode > ./bin/journald-cloudwatch-logs-xenial
 sha256sum ./bin/journald-cloudwatch-logs-xenial
 gzip -k9 ./bin/journald-cloudwatch-logs-xenial
